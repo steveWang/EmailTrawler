4bcb4fb58b094ff4393e1bc9fa9b45fc62020328
Steve Wang <steve.john.wang@gmail.com>
2016-09-13 11:02:41 -0400

    Implement bulk of trawler code.
    All that remains at this point is to add handling of javascript-based
    redirects, if that's even a reasonable thing to do.

--- trawler.py
+++ trawler.py
@@ -4,31 +4,51 @@ from urllib.parse import urlparse, urljoin
 import sys
 import re
 
+LOCAL_HELPER = r'[a-zA-Z!#$%&\'*+-/=?^_`|~]+'
+LOCALPART = r'(?:(?:%s\.)*%s|"(?:[a-zA-Z!#$%%&\'*+-/=?^_`{|}~.]+)")' % (LOCAL_HELPER, LOCAL_HELPER)
+LABEL = r'(?!-)[a-zA-Z0-9\-]{1,63}(?<!-)'
+DOMAINPART = r'(?:%s\.)+%s' % (LABEL, LABEL)
+
+# Ignore IP address literals and non-canonicalized addresses.
+
+# To cut down on false positives, this regex deviates a bit from the email
+# address specification by creating more false negatives -- for instance,
+# it requires at least two labels in a domain name, and braces (i.e. {})
+# are not valid characters in an email address.
+EMAIL_PATTERN = re.compile('%s@%s' % (LOCALPART, DOMAINPART))
+
 class Scraper():
     def __init__(self):
         self.s = requests.Session()
 
     def go(self, url):
-        return BeautifulSoup(self.s.get(url).text, 'html.parser')
+        return self.s.get(url).text
 
-# This doesn't override existing schemes.
+scraper = Scraper()
+
+# Don't override specified schemes.
 start_url = sys.argv[1]
 if start_url.find('://') == -1:
     start_url = 'http://%s' % start_url
 
-origin = urlparse(start_url).netloc
+# Set start_url and consequently origin based on redirects.
+start_url = scraper.s.get(start_url).url
 
-scraper = Scraper()
+origin = urlparse(start_url).netloc
 
 queue = [start_url]
 seen_pages = set(queue)
+seen_emails = set()
 
 # DFS to explore unseen URLs. [].append and [].pop implement this for us.
 while len(queue):
     url = queue.pop()
     text = scraper.go(url)
 
-    for a in text.findAll('a'):
+    seen_emails |= set(EMAIL_PATTERN.findall(text))
+
+    soup = BeautifulSoup(text, 'html.parser')
+    for a in soup.findAll('a'):
         if not a.has_attr('href'):
             continue
         linked_url = urljoin(url, a['href'])
@@ -38,4 +58,4 @@ while len(queue):
             seen_pages.add(linked_url)
             queue.append(linked_url)
 
-print(seen_pages)
+print('\n'.join(seen_emails))
